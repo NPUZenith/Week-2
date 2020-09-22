93c4746d7d1c82b2bdbfec2d2cc49932075f1a22
NPUZenith <58347499+NPUZenith@users.noreply.github.com>
2020-09-21 20:56:26 -0400

    Week 2 - Programming Workflow

---
+++ src/AboutMe.java
@@ -0,0 +1,17 @@
+// Copyright NPUZenith
+
+public class AboutMe {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        String name = myName();
+
+        String output = "My name is " + name;
+        System.out.println(output);
+    }
+
+    public static String myName(){
+        return "Zenith";
+    }
+}
